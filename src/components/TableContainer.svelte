dc2373e8d2a375834e766a75cb73dbcee540843c
shaw8wit <shaw8wit@gmail.com>
2020-10-29 20:52:11 +0530

    components fixed

---
+++ src/components/TableContainer.svelte
@@ -0,0 +1,10 @@
+<script>
+    import TableFilter from "./TableFilter.svelte";
+    import Table from "./Table.svelte";
+</script>
+
+<h1>Table container</h1>
+
+<TableFilter />
+
+<Table />
--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,11 +1,18 @@
+<script context="module">
+    export async function preload(page) {
+        return { state: page.params["state"] };
+    }
+</script>
+
 <script>
     import Stat from "../components/Stat.svelte";
     import Chart from "../components/Chart.svelte";
     import TableContainer from "../components/TableContainer.svelte";
+    export let state;
 </script>
+
+<h1>{state}</h1>
 
 <Stat />
 
 <Chart />
-
-<TableContainer />

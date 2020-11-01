329ae92ba555c4569062c3864cd016bf3d4c0e5f
shaw8wit <shaw8wit@gmail.com>
2020-11-01 15:03:52 +0530

    code refactoring

--- src/routes/[country].svelte
+++ src/routes/[country].svelte
@@ -1,6 +1,6 @@
 <script context="module">
     export async function preload(page) {
-        return { state: page.params["state"] };
+        return { country: page.params["country"] };
     }
 </script>
 
@@ -8,16 +8,16 @@
     import Stat from "../components/Stat.svelte";
     import Chart from "../components/Chart.svelte";
     import TableContainer from "../components/TableContainer.svelte";
-    export let state;
+    export let country;
 </script>
 
 <svelte:head>
-    <title>Covid Tracker Info - {state}</title>
+    <title>Covid Tracker Info - {country}</title>
 </svelte:head>
 
 <div class="section header">
     <div class="container">
-        <h1>Covid 19 - {state}</h1>
+        <h1>Covid 19 - {country}</h1>
     </div>
 </div>
 
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,9 +7,11 @@
         <h1>About</h1>
         <p>
             The data for this project came from:
-            <a href="/">Here</a>
+            <a href="https://documenter.getpostman.com/view/10808728/SzS8rjbc">
+                Coronavirus COVID19 API
+            </a>
             <br />
-            Learn more about it by visiting their website.
+            Learn more about it by visiting their documentation on Postman.
         </p>
     </div>
 </div>
